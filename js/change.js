9738af8215ab43df687ae8d0882ece47cb6ab6c3
Oleg  Sova <sovaoleg82@gmail.com>
2018-11-25 11:05:29 +0200

    change

--- js/change.js
+++ js/change.js
@@ -1,20 +1,22 @@
-const listpoint = document.getElementById('listt');
+const listpoint = document.getElementsByClassName('listt');
 
-console.log(listpoint);
 
-listpoint.onmouseover = function(){
-	document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Sovaoleg/Sovaoleg.github.io/master/opt_img/bgeye.png')";
-	document.getElementsByClassName('tree')[0].style.visibility='hidden';
-	document.getElementsByClassName('main-bl')[0].style.display='none';
-	document.getElementsByClassName('star')[0].style.display='none';
-	document.getElementsByClassName('header')[0].style.visibility='hidden';
+for ( let i = 0; i < listpoint.length; i++)
+{
+	listpoint[i].onmouseover = function(){
+		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Sovaoleg/Sovaoleg.github.io/master/opt_img/bgeye.png')";
+		document.getElementsByClassName('tree')[0].style.visibility='hidden';
+		document.getElementsByClassName('main-bl')[0].style.display='none';
+		document.getElementsByClassName('star')[0].style.display='none';
+		document.getElementsByClassName('header')[0].style.visibility='hidden';
+	}
+	listpoint[i].onmouseout = function(){
+		document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Sovaoleg/Sovaoleg.github.io/master/opt_img/main1.png')";
+		document.getElementsByClassName('tree')[0].style.visibility='visible';
+		document.getElementsByClassName('main-bl')[0].style.display='block';
+		document.getElementsByClassName('star')[0].style.display='block';
+		document.getElementsByClassName('header')[0].style.visibility='visible';
+	}
 }
 
-listpoint.onmouseout = function(){
-	document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Sovaoleg/Sovaoleg.github.io/master/opt_img/main1.png')";
-	document.getElementsByClassName('tree')[0].style.visibility='visible';
-	document.getElementsByClassName('main-bl')[0].style.display='block';
-	document.getElementsByClassName('star')[0].style.display='block';
-	document.getElementsByClassName('header')[0].style.visibility='visible';
-}
 
