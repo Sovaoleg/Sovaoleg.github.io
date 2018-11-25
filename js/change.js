4877b45a65cbbac57b7f0c92db2597f4aa8a7b7c
Oleg  Sova <sovaoleg82@gmail.com>
2018-11-25 10:39:56 +0200

    path

--- js/change.js
+++ js/change.js
@@ -3,7 +3,7 @@ const listpoint = document.getElementById('listt');
 console.log(listpoint);
 
 listpoint.onmouseover = function(){
-	document.body.style.backgroundImage = "url('../portfolio/opt_img/bgeye.png')";
+	document.body.style.backgroundImage = "url('https://raw.githubusercontent.com/Sovaoleg/Sovaoleg.github.io/master/opt_img/bgeye.png')";
 	document.getElementsByClassName('tree')[0].style.visibility='hidden';
 	document.getElementsByClassName('main-bl')[0].style.display='none';
 	document.getElementsByClassName('star')[0].style.display='none';
