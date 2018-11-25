0b346fd062b1f99d962df82057dae8c5f4fe168b
Oleg  Sova <sovaoleg82@gmail.com>
2018-11-25 10:54:44 +0300

    new pt

---
+++ js/change.js
@@ -0,0 +1,20 @@
+const listpoint = document.getElementById('listt');
+
+console.log(listpoint);
+
+listpoint.onmouseover = function(){
+	document.body.style.backgroundImage = "url('../portfolio/opt_img/bgeye.png')";
+	document.getElementsByClassName('tree')[0].style.visibility='hidden';
+	document.getElementsByClassName('main-bl')[0].style.display='none';
+	document.getElementsByClassName('star')[0].style.display='none';
+	document.getElementsByClassName('header')[0].style.visibility='hidden';
+}
+
+listpoint.onmouseout = function(){
+	document.body.style.backgroundImage = "url('../portfolio/opt_img/main1.png')";
+	document.getElementsByClassName('tree')[0].style.visibility='visible';
+	document.getElementsByClassName('main-bl')[0].style.display='block';
+	document.getElementsByClassName('star')[0].style.display='block';
+	document.getElementsByClassName('header')[0].style.visibility='visible';
+}
+
